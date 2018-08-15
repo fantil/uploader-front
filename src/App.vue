62dafeaad2f15b5bf57a8b8731238cc9e5522532
范天凯 <ftk@servyou.com.cn>
2018-08-15 19:29:58 +0800

    todolist completed

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <input v-model="inputValue" />
+    <button @click="handleSubmit">添加</button>
+    <ul>
+      <todo-item v-for="(item, index) of list"
+                 :key="index"
+                 :content="item"
+                 :index="index"
+                 @deleteItem="handleDelete">
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  name: 'App',
+  components: {
+      'todo-item': TodoItem
+  },
+  data () {
+      return {
+          inputValue: '',
+          list: []
+      }
+  },
+  methods: {
+      handleSubmit() {
+          this.list.push(this.inputValue);
+          this.inputValue = ''
+      },
+      handleDelete(index) {
+          this.list.splice(index, 1)
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
