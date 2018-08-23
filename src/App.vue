c62e7a0c53fc91f744e9b80b94612b844f597fef
范天凯 <ftk@servyou.com.cn>
2018-08-23 21:57:00 +0800

    可运行

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,42 @@
 <template>
-  <div id="app">
-    <input v-model="inputValue" />
-    <button @click="handleSubmit">添加</button>
-    <ul>
-      <todo-item v-for="(item, index) of list"
-                 :key="index"
-                 :content="item"
-                 :index="index"
-                 @deleteItem="handleDelete">
-      </todo-item>
-    </ul>
-    <uploader></uploader>
-  </div>
+    <div id="app">
+        <myUploader></myUploader>
+    </div>
 </template>
 
 <script>
-import TodoItem from './components/TodoItem'
-import Uploader from './components/Uploader'
+import MyUploader from './components/MyUploader'
 
 export default {
-  name: 'App',
-  components: {
-      'todo-item': TodoItem,
-      'uploader': Uploader
-  },
-  data () {
-      return {
+    name: 'App',
+    components: {
+        myUploader: MyUploader
+    },
+    data () {
+        return {
           inputValue: '',
           list: []
-      }
-  },
-  methods: {
-      handleSubmit() {
-          this.list.push(this.inputValue);
-          this.inputValue = ''
-      },
-      handleDelete(index) {
-          this.list.splice(index, 1)
-      }
-  }
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.list.push(this.inputValue);
+            this.inputValue = ''
+        },
+        handleDelete(index) {
+            this.list.splice(index, 1)
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import ElementUI from 'element-ui'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import uploader from 'vue-simple-uploader';
+import App from './App';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App'
 
 Vue.config.productionTip = false
 
 
 Vue.use(ElementUI)
+Vue.use(uploader)
 
 /* eslint-disable no-new */
 new Vue({
